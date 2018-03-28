99c77b6fac471fed433a7e422bc125d505cba241
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-03-28 14:04:00 +0000

    Partially implement IEngineAPI and IGameUIFuncs interfaces

--- goldsrc/engine/sys_dll2.cpp
+++ goldsrc/engine/sys_dll2.cpp
@@ -1,10 +1,126 @@
 /// @file
-/// @brief Dedicated Server API implementation
+/// @brief Listen and Dedicated Server API implementation
 
 #include "quakedef.h"
+#include "engine_launcher_api.h"
 #include "engine_hlds_api.h"
+#include "igameuifuncs.h"
 #include "iengine.h"
-#include "Interface.hpp"
+#include "interface.h"
+
+int RunListenServer(void *instance, char *basedir, char *cmdline, char *postRestartCmdLineArgs, CreateInterfaceFn launcherFactory, CreateInterfaceFn filesystemFactory)
+{
+	// TODO: Whole bunch of Sys_Init* calls
+	
+	// TODO: CWindowManager::CreateGameWindow(); // IGame::CreateGameWindow
+	
+	if(!gpEngine->Load(false, basedir, cmdline))
+		return EXIT_FAILURE;
+	
+	while(true)
+		gpEngine->Frame();
+	
+	gpEngine->Unload();
+	
+	// TODO: CWindowManager::DestroyGameWindow(); // IGame::DestroyGameWindow
+	
+	// TODO: Whole bunch of Sys_Shutdown* calls
+	
+	return EXIT_SUCCESS;
+};
+
+class CEngineAPI final : public IEngineAPI
+{
+public:
+	CEngineAPI();
+	~CEngineAPI();
+	
+	int Run(void *instance, char *basedir, char *cmdline, char *postRestartCmdLineArgs, CreateInterfaceFn launcherFactory, CreateInterfaceFn filesystemFactory) override;
+};
+
+EXPOSE_SINGLE_INTERFACE(CEngineAPI, IEngineAPI, VENGINE_LAUNCHER_API_VERSION);
+
+int CEngineAPI::Run(void *instance, char *basedir, char *cmdline, char *postRestartCmdLineArgs, CreateInterfaceFn launcherFactory, CreateInterfaceFn filesystemFactory)
+{
+	return RunListenServer(instance, basedir, cmdline, postRestartCmdLineArgs, launcherFactory, filesystemFactory);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class CGameUIFuncs final : public IGameUIFuncs
+{
+public:
+	CGameUIFuncs();
+	~CGameUIFuncs();
+	
+	bool IsKeyDown(char const *keyname, bool &isdown) override;
+
+	const char *Key_NameForKey(int keynum) override;
+	const char *Key_BindingForKey(int keynum) override;
+
+	vgui2::KeyCode GetVGUI2KeyCodeForBind(const char *bind) override;
+
+	void GetVideoModes(struct vmode_s **liststart, int *count) override;
+	void GetCurrentVideoMode(int *wide, int *tall, int *bpp) override;
+
+	void GetCurrentRenderer(char *name, int namelen,
+	                                int *windowed, int *hdmodels,
+	                                int *addons_folder, int *vid_level) override;
+	//void GetCurrentRenderer(char *name, int namelen, int *windowed) override;
+
+	bool IsConnectedToVACSecureServer() override;
+
+	int Key_KeyStringToKeyNum(const char *string) override;
+};
+
+EXPOSE_SINGLE_INTERFACE(CGameUIFuncs, IGameUIFuncs, GAMEUIFUNCS_INTERFACE_VERSION);
+
+CGameUIFuncs::CGameUIFuncs() = default;
+CGameUIFuncs::~CGameUIFuncs() = default;
+
+bool CGameUIFuncs::IsKeyDown(char const *keyname, bool &isdown)
+{
+	return Key_IsDown(keyname, isdown);
+};
+
+const char *CGameUIFuncs::Key_NameForKey(int keynum)
+{
+	return Key_NameForKey(keynum);
+};
+
+const char *CGameUIFuncs::Key_BindingForKey(int keynum)
+{
+	return Key_BindingForKey(keynum);
+};
+
+vgui::KeyCode CGameUIFuncs::GetVGUI2KeyCodeForBind(const char *bind)
+{
+	return 0;
+};
+
+void CGameUIFuncs::GetVideoModes(struct vmode_s **liststart, int *count)
+{
+};
+
+void CGameUIFuncs::GetCurrentVideoMode(int *wide, int *tall, int *bpp)
+{
+};
+
+void CGameUIFuncs::GetCurrentRenderer(char *name, int namelen, int *windowed, int *hdmodels, int *addons_folder, int *vid_level)
+{
+};
+
+bool CGameUIFuncs::IsConnectedToVACSecureServer()
+{
+	return false;
+};
+
+int CGameUIFuncs::Key_KeyStringToKeyNum(const char *string)
+{
+	return Key_KeyStringToKeyNum(string);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class CDedicatedServerAPI final : public IDedicatedServerAPI
 {
@@ -29,11 +145,16 @@ CDedicatedServerAPI::~CDedicatedServerAPI() = default;
 
 bool CDedicatedServerAPI::Init(char *basedir, char *cmdline, CreateInterfaceFn launcherFactory, CreateInterfaceFn filesystemFactory)
 {
+	if(!gpEngine->Load(true, basedir, cmdline))
+		return false;
+	
 	return true;
 };
 
 int CDedicatedServerAPI::Shutdown()
 {
+	gpEngine->Unload();
+	
 	return 0;
 };
 
