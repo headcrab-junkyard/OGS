09b873675a6302ddac1aab943558c450d2b20427
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-03-28 14:27:50 +0000

    engine/filesystem.h/.cpp -> engine/filesystem_internal.h/.cpp (see SDK's public/FileSystem.h for details)

---
+++ goldsrc/engine/filesystem_internal.cpp
@@ -0,0 +1,83 @@
+/// @file
+
+#include "quakedef.h"
+#include "IFileSystem.hpp"
+
+IFileSystem *gpFileSystem{nullptr};
+
+void FileSystem_Init(char *basedir, void *filesystemFactory)
+{
+	// TODO
+	
+	if(!basedir || !*basedir)
+		return;
+	
+	if(!filesystemFactory)
+		return;
+	
+	gpFileSystem = (IFileSystem*)(((CreateInterfaceFn)filesystemFactory)(FILESYSTEM_INTERFACE_VERSION, nullptr));
+	
+	if(!gpFileSystem)
+		return;
+};
+
+void FileSystem_Shutdown()
+{
+	// TODO
+	
+	gpFileSystem = nullptr;
+};
+
+/*
+===============================================================================
+
+FILE IO
+
+===============================================================================
+*/
+
+int FS_FileOpenRead(const char *path, int *hndl)
+{
+	return 0; // TODO
+};
+
+int FS_FileOpenWrite(const char *path)
+{
+	return gpFileSystem->FileOpen(path, "wb");
+};
+
+void FS_FileClose(int handle)
+{
+	gpFileSystem->FileClose(handle);
+};
+
+void FS_FileSeek(int handle, int position)
+{
+	gpFileSystem->FileSeek(handle, position);
+};
+
+int FS_FileRead(int handle, void *dest, int count)
+{
+	return gpFileSystem->FileRead(handle, dest, count);
+};
+
+int FS_FileWrite(int handle, const void *data, int count)
+{
+	return gpFileSystem->FileWrite(handle, data, count);
+};
+
+int FS_FileTime(const char *path)
+{
+	return gpFileSystem->GetFileTime(path);
+};
+
+void FS_mkdir(const char *path)
+{
+	// TODO: if not dedicated?
+
+#ifdef _WIN32
+	_mkdir(path);
+#else
+	mkdir(path, 0777);
+#endif
+};
---
+++ goldsrc/engine/filesystem_internal.h
@@ -0,0 +1,62 @@
+/// @file
+
+//
+// file IO
+//
+
+#pragma once
+
+#define FILESYSTEM_INTERNAL_H
+
+#define FILESYSTEM_INVALID_HANDLE (FileHandle_t)0
+
+typedef enum
+{
+	FILESYSTEM_SEEK_HEAD = 0,
+	FILESYSTEM_SEEK_CURRENT,
+	FILESYSTEM_SEEK_TAIL
+	//...?
+} FileSystemSeek_t;
+
+enum
+{
+	FILESYSTEM_INVALID_FIND_HANDLE = -1
+};
+
+typedef enum
+{
+	FILESYSTEM_WARNING_QUIET = 0,      ///< Don't print anything
+	FILESYSTEM_WARNING_REPORTUNCLOSED, ///< On shutdown, report names of files left unclosed
+	FILESYSTEM_WARNING_REPORTUSAGE,    ///< Report number of times a file was opened/closed
+	FILESYSTEM_WARNING_REPORTALLACCESS ///< Report all open/close events to console (SLOW!!!!)
+} FileWarningLevel_t;
+
+
+////////////
+// TODO: here or somewhere else?????
+
+void FileSystem_Init(char *basedir, void *filesystemFactory); // TODO: return bool/int?
+void FileSystem_Shutdown();
+////////////
+
+/// returns the file size
+/// return -1 if file is not present
+/// the file should be in BINARY mode for stupid OSs that care
+int FS_FileOpenRead(const char *path, int *hndl);
+
+int FS_FileOpenWrite(const char *path);
+
+void FS_FileClose(int handle);
+
+void FS_FileSeek(int handle, int position);
+
+int FS_FileRead(int handle, void *dest, int count);
+int FS_FileWrite(int handle, const void *data, int count);
+
+int	FS_FileTime(const char *path);
+
+void FS_mkdir(const char *path);
+
+// Dynamic Library Management
+	
+//void *FS_LoadLibrary(const char *name, const char *path);
