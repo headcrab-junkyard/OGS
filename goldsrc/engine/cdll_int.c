eb9a46e09dc7fb0f5e8d3bfa6aaf3376ea80eb5e
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-03-31 13:12:31 +0000

    Update cdll_int.c; Define and partially implement all ClientDLL_* declarations

--- goldsrc/engine/cdll_int.c
+++ goldsrc/engine/cdll_int.c
@@ -10,55 +10,246 @@
 
 #include "quakedef.h"
 
-static cl_enginefunc_t gEngFuncs; // TODO: name overlap with server-side version?
-cldll_func_t gClientDLL;
+static cl_enginefunc_t gEngFuncs; // TODO: name overlap with server-side version? cl_enginefuncs in GS
+cldll_func_t cl_funcs;
 
-typedef int /*CL_DLLEXPORT*/ (*fnF)(void *pv);
+typedef int /*CL_DLLEXPORT*/ (*pfnF)(void *pv);
 
 void ClientDLL_Init()
 {
-	if(gClientDLL.pfnInit)
-		gClientDLL.pfnInit();
+	// TODO
+	ClientDLL_HudInit();
+	ClientDLL_HudVidInit();
+	ClientDLL_ClientMoveInit();
 };
 
-int ClientDLL_VidInit()
-{
-	if(gClientDLL.pfnVidInit)
-		return gClientDLL.pfnVidInit();
-	
-	return 0;
-};
-
-/*
 void ClientDLL_Shutdown()
 {
-	if(gClientDLL.pfnShutdown)
-		gClientDLL.pfnShutdown();
+	//if(cl_funcs.pfnShutdown)
+		//cl_funcs.pfnShutdown();
 };
-*/
+
+void ClientDLL_HudInit()
+{
+	if(cl_funcs.pfnHudInit)
+		cl_funcs.pfnHudInit();
+};
+
+void ClientDLL_HudVidInit()
+{
+	if(cl_funcs.pfnHudVidInit)
+		cl_funcs.pfnHudVidInit();
+};
+
+void ClientDLL_UpdateClientData()
+{
+	// TODO
+};
 
 void ClientDLL_Frame(double time)
 {
-	if(gClientDLL.pfnFrame)
-		gClientDLL.pfnFrame(time);
+	if(cl_funcs.pfnFrame)
+		cl_funcs.pfnFrame(time);
 };
 
-int ClientDLL_Redraw(float time, int intermission)
+void ClientDLL_HudRedraw(int intermission)
 {
-	if(gClientDLL.pfnRedraw)
-		return gClientDLL.pfnRedraw(time, intermission);
+	if(cl_funcs.pfnHudRedraw)
+		cl_funcs.pfnHudRedraw(intermission);
+};
+
+void ClientDLL_MoveClient(struct playermove_s *ppmove)
+{
+	// TODO
+};
+
+void ClientDLL_ClientMoveInit(struct playermove_s *ppmove)
+{
+	// TODO
+};
+
+char ClientDLL_ClientTextureType(char *name)
+{
+	// TODO
+	return '\0';
+};
+
+void ClientDLL_CreateMove(float frametime, struct usercmd_s *cmd, int active)
+{
+	// TODO
+};
+
+void ClientDLL_ActivateMouse()
+{
+	if(cl_funcs.pfnActivateMouse)
+		cl_funcs.pfnActivateMouse();
+};
+
+void ClientDLL_DeactivateMouse()
+{
+	if(cl_funcs.pfnDeactivateMouse)
+		cl_funcs.pfnDeactivateMouse();
+};
+
+void ClientDLL_MouseEvent(int mstate)
+{
+	if(cl_funcs.pfnMouseEvent)
+		cl_funcs.pfnMouseEvent(mstate);
+};
+
+void ClientDLL_ClearStates()
+{
+	if(cl_funcs.pfnClearStates)
+		cl_funcs.pfnClearStates();
+};
+
+int ClientDLL_IsThirdPerson()
+{
+	// TODO
+	return 0;
+};
+
+void ClientDLL_GetCameraOffsets(float *ofs)
+{
+	// TODO
+};
+
+int ClientDLL_GraphKeyDown()
+{
+	// TODO
+	return 0;
+};
+
+struct kbutton_s *ClientDLL_FindKey(const char *name)
+{
+	// TODO
+	return NULL;
+};
+
+void ClientDLL_CAM_Think()
+{
+	// TODO
+};
+
+void ClientDLL_IN_Accumulate()
+{
+	if(cl_funcs.pfnIN_Accumulate)
+		cl_funcs.pfnIN_Accumulate();
+};
+
+void ClientDLL_CalcRefdef(struct ref_params_s *pparams)
+{
+	// TODO
+};
+
+int ClientDLL_AddEntity(int type, struct cl_entity_s *ent)
+{
+	// TODO
+	return 0;
+};
+
+void ClientDLL_CreateEntities()
+{
+	// TODO
+};
+
+void ClientDLL_DrawNormalTriangles()
+{
+	// TODO
+};
+
+void ClientDLL_DrawTransparentTriangles()
+{
+	// TODO
+};
+
+void ClientDLL_StudioEvent(const struct mstudioevent_s *event, const struct cl_entity_s *entity)
+{
+	// TODO
+};
+
+void ClientDLL_PostRunCmd(struct local_state_s *from, struct local_state_s *to, struct usercmd_s *cmd, int runfuncs, double time, uint random_seed)
+{
+	// TODO
+};
+
+void ClientDLL_TxferLocalOverrides(struct entity_state_s *state, const struct clientdata_s *client)
+{
+	// TODO
+};
+
+void ClientDLL_ProcessPlayerState(struct entity_state_s *dst, const struct entity_state_s *src)
+{
+	// TODO
+};
+
+void ClientDLL_TxferPredictionData(struct entity_state_s *ps, const struct entity_state_s *pps, struct clientdata_s *pcd, const struct clientdata_s *ppcd, struct weapon_data_s *wd, const struct weapon_data_s *pwd)
+{
+	// TODO
+};
+
+void ClientDLL_ReadDemoBuffer(int size, byte *buffer)
+{
+	// TODO
+};
+
+int ClientDLL_ConnectionlessPacket(const struct netadr_s *net_from, const char *args, char *response_buffer, int *response_buffer_size)
+{
+	// TODO
+	return 0;
+};
+
+int ClientDLL_GetHullBounds(int hullnumber, float *mins, float *maxs)
+{
+	// TODO
+	return 0;
+};
+
+void ClientDLL_VGui_ConsolePrint(const char *text)
+{
+	// TODO
+};
+
+int ClientDLL_Key_Event(int down, int keynum, const char *pszCurrentBinding)
+{
+	if(cl_funcs.pfnKey_Event)
+		return cl_funcs.pfnKey_Event(down, keynum, pszCurrentBinding);
 	
 	return 0;
 };
 
-qboolean LoadClientDLLviaF()
+void ClientDLL_TempEntUpdate(double ft, double ct, double grav, struct tempent_s **ppFreeTE, struct tempent_s **ppActiveTE, int (*addEntity)(struct cl_entity_s *pEntity), void (*playTESound)(struct tempent_s *pTemp, float damp))
 {
-	// TODO: per-export loading as a fallback
-	
+	// TODO
+};
+
+struct cl_entity_s *ClientDLL_GetUserEntity(int index)
+{
+	// TODO
+	return NULL;
+};
+
+void ClientDLL_VoiceStatus(int entindex, qboolean bTalking)
+{
+	// TODO
+};
+
+void ClientDLL_DirectorMessage(int iSize, void *pbuf)
+{
+	// TODO
+};
+
+void ClientDLL_ChatInputPosition(int *x, int *y)
+{
+	// TODO
+};
+
+qboolean LoadClientDLLF()
+{
 	pfnF fnF = NULL;
 	
 	//memcpy(&gEngFuncs, 0, sizeof(cl_enginefunc_t));
-	//memcpy(&gClientDLL, 0, sizeof(cldll_func_t));
+	//memcpy(&cl_funcs, 0, sizeof(cldll_func_t));
 	
 	void *pClientDLL = Sys_LoadModule("valve/cl_dll/client"); // TODO: FS_LoadLibrary or something else that will load it from the game folder instead of app folder
 	
@@ -70,9 +261,9 @@ qboolean LoadClientDLLviaF()
 	if(!fnF)
 		return false;
 	
-	fnF(&gClientDLL);
+	fnF(&cl_funcs);
 	
-	if(!gClientDLL.Initialize(&gEngFuncs, CLDLL_INTERFACE_VERSION)) // TODO: So.... Are you alive?
+	if(!cl_funcs.pfnInitialize(&gEngFuncs, CLDLL_INTERFACE_VERSION)) // TODO: So.... Are you alive?
 		return false;
 	
 	return true;
@@ -80,6 +271,8 @@ qboolean LoadClientDLLviaF()
 
 void LoadClientDLL()
 {
-	if(!LoadClientDLLviaF()
+	// TODO: per-export loading as a fallback
+	
+	if(!LoadClientDLLF())
 		return;
 };
