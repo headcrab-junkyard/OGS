32258e7b25526a3bd66d897b0ef050c156249f51
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-04-02 14:16:51 +0000

    Update 'goldsrc/engine/quakedef.h'

--- goldsrc/engine/quakedef.h
+++ goldsrc/engine/quakedef.h
@@ -1,5 +1,6 @@
 /*
 Copyright (C) 1996-1997 Id Software, Inc.
+Copyright (C) 2018 Headcrab Garage
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -19,7 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 /// @file
-/// @brief primary header for client
+/// @brief primary header for both client and server
 
 //#define	GLTEST			// experimental stuff
 
@@ -42,6 +43,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <stdio.h>
 #include <stdlib.h>
 #include <setjmp.h>
+//#include <ctype.h>
 
 #if defined(_WIN32) && !defined(WINDED)
 
@@ -65,6 +67,10 @@ void	VID_UnlockBuffer ();
 #define id386	0
 #endif
 
+#ifdef SERVERONLY		// no asm in dedicated server
+#undef id386
+#endif
+
 #if id386
 #define UNALIGNED_OK	1	// set to 0 if unaligned accesses are not supported
 #else
@@ -77,7 +83,7 @@ void	VID_UnlockBuffer ();
 #define UNUSED(x)	(x = x)	// for pesky compiler / lint warnings
 
 #define	MINIMUM_MEMORY			0x550000
-#define	MINIMUM_MEMORY_LEVELPAK	(MINIMUM_MEMORY + 0x100000)
+//#define	MINIMUM_MEMORY_LEVELPAK	(MINIMUM_MEMORY + 0x100000) // TODO: unused
 
 #define MAX_NUM_ARGVS	50
 
@@ -133,68 +139,45 @@ void	VID_UnlockBuffer ();
 
 // stock defines
 
+//
+// item flags
+//
 #define	IT_SHOTGUN				1
 #define	IT_SUPER_SHOTGUN		2
 #define	IT_NAILGUN				4
 #define	IT_SUPER_NAILGUN		8
+
 #define	IT_GRENADE_LAUNCHER		16
 #define	IT_ROCKET_LAUNCHER		32
 #define	IT_LIGHTNING			64
 #define IT_SUPER_LIGHTNING      128
+
 #define IT_SHELLS               256
 #define IT_NAILS                512
 #define IT_ROCKETS              1024
 #define IT_CELLS                2048
+
 #define IT_AXE                  4096
+
 #define IT_ARMOR1               8192
 #define IT_ARMOR2               16384
 #define IT_ARMOR3               32768
+
 #define IT_SUPERHEALTH          65536
+
 #define IT_KEY1                 131072
 #define IT_KEY2                 262144
+
 #define	IT_INVISIBILITY			524288
+
 #define	IT_INVULNERABILITY		1048576
 #define	IT_SUIT					2097152
 #define	IT_QUAD					4194304
+
 #define IT_SIGIL1               (1<<28)
 #define IT_SIGIL2               (1<<29)
 #define IT_SIGIL3               (1<<30)
 #define IT_SIGIL4               (1<<31)
-
-//===========================================
-//rogue changed and added defines
-
-#define RIT_SHELLS              128
-#define RIT_NAILS               256
-#define RIT_ROCKETS             512
-#define RIT_CELLS               1024
-#define RIT_AXE                 2048
-#define RIT_LAVA_NAILGUN        4096
-#define RIT_LAVA_SUPER_NAILGUN  8192
-#define RIT_MULTI_GRENADE       16384
-#define RIT_MULTI_ROCKET        32768
-#define RIT_PLASMA_GUN          65536
-#define RIT_ARMOR1              8388608
-#define RIT_ARMOR2              16777216
-#define RIT_ARMOR3              33554432
-#define RIT_LAVA_NAILS          67108864
-#define RIT_PLASMA_AMMO         134217728
-#define RIT_MULTI_ROCKETS       268435456
-#define RIT_SHIELD              536870912
-#define RIT_ANTIGRAV            1073741824
-#define RIT_SUPERHEALTH         2147483648
-
-//MED 01/04/97 added hipnotic defines
-//===========================================
-//hipnotic added defines
-#define HIT_PROXIMITY_GUN_BIT 16
-#define HIT_MJOLNIR_BIT       7
-#define HIT_LASER_CANNON_BIT  23
-#define HIT_PROXIMITY_GUN   (1<<HIT_PROXIMITY_GUN_BIT)
-#define HIT_MJOLNIR         (1<<HIT_MJOLNIR_BIT)
-#define HIT_LASER_CANNON    (1<<HIT_LASER_CANNON_BIT)
-#define HIT_WETSUIT         (1<<(23+2))
-#define HIT_EMPATHY_SHIELDS (1<<(23+3))
 
 //===========================================
 
@@ -211,7 +194,7 @@ extern "C"
 #include "bspfile.h"
 #include "vid.h"
 #include "sys.h"
-#include "filesystem.h"
+#include "filesystem_internal.h"
 #include "zone.h"
 #include "mathlib.h"
 #include "info.h"
@@ -232,6 +215,7 @@ extern "C"
 #include "progs.h"
 #include "server.h"
 #include "eiface.h"
+#include "cdll_int.h"
 
 #ifdef GLQUAKE
 #include "gl_model.h"
@@ -294,7 +278,6 @@ extern	double		realtime;			// not bounded in any way, changed at
 										// start of every frame, never reset
 
 void Host_ClearMemory ();
-void Host_ServerFrame ();
 void Host_InitCommands ();
 void Host_Init (quakeparms_t *parms);
 void Host_Shutdown();
@@ -304,6 +287,7 @@ void Host_Frame (float time); // TODO: state, stateinfo
 void Host_Quit_f ();
 void Host_ClientCommands (const char *fmt, ...);
 void Host_ShutdownServer (qboolean crash);
+//void Host_WriteConfiguration ();
 
 extern qboolean		msg_suppress_1;		// suppresses resolution and cache size console output
 										//  an fullscreen DIB focus gain/loss
