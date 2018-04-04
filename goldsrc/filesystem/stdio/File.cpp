5dd43e06e26f6d84472c7e94125b2b4bbbd82b2d
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-04-04 14:29:07 +0000

    Partially implement the IFile interface

--- goldsrc/filesystem/stdio/File.cpp
+++ goldsrc/filesystem/stdio/File.cpp
@@ -2,5 +2,83 @@
 
 #include "File.hpp"
 
-CFile::CFile() = default;
-CFile::~CFile() = default;
+CFile::CFile(const char *asName) : msName(asName), mpHandle(fopen(asName))
+{
+	assert(mpHandle);
+	
+	//if(!mpHandle)
+		//std::runtime_error();
+};
+
+CFile::~CFile()
+{
+	if(mpHandle)
+	{
+		fflush(mpHandle);
+		fclose(mpHandle);
+	};
+};
+
+void CFile::Write(const void *data, int count)
+{
+	if(mpHandle)
+		fwrite(data, sizeof(char), count, mpHandle);
+};
+
+int CFile::Read(void *dest, int count) const
+{
+	if(mpHandle)
+		return fread(dest, sizeof(char), count, mpHandle);
+
+	return 0;
+};
+
+void CFile::Printf(const char *text, ...)
+{
+	if(mpHandle)
+		fputs(text, mpHandle);
+};
+
+int CFile::Seek(int position) const
+{
+	if(mpHandle)
+		return fseek(mpHandle, position, SEEK_SET);
+
+	return 0;
+};
+
+int CFile::Tell() const
+{
+	if(mpHandle)
+		return ftell(mpHandle);
+
+	return 0;
+};
+
+const char *CFile::GetName() const
+{
+	if(mpHandle)
+		return msName;
+
+	return "";
+};
+
+const char *CFile::GetExt() const
+{
+	return ""; // TODO
+};
+
+const char *CFile::GetPath() const
+{
+	return ""; // TODO
+};
+
+int CFile::GetTime() const
+{
+	return 0; // TODO
+};
+
+int CFile::GetSize() const
+{
+	return 0; // TODO
+};
--- goldsrc/filesystem/stdio/File.hpp
+++ goldsrc/filesystem/stdio/File.hpp
@@ -2,11 +2,32 @@
 
 #pragma once
 
+#include <cstdio>
 #include "IFile.hpp"
 
 class CFile final : public IFile
 {
 public:
-	CFile();
+	CFile(const char *asName);
 	~CFile();
+	
+	void Write(const void *data, int count) override;
+	int Read(void *dest, int count) const override;
+
+	void Printf(const char *text, ...) override;
+
+	int Seek(int position) const override;
+
+	int Tell() const override;
+
+	const char *GetName() const override;
+	const char *GetExt() const override;
+	const char *GetPath() const override;
+
+	int GetTime() const override;
+	int GetSize() const override;
+private:
+	const char *msName{""};
+	
+	FILE *mpHandle{nullptr};
 };
