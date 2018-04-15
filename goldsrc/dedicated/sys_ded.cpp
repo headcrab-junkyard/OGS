dac3ed3729e84131c1f9b07de31e2b9fc2c4b943
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-04-15 09:32:24 +0000

    Update dedicated launcher code; Add license headers and proper error handling (engine/fs modules loading failures)

--- goldsrc/dedicated/sys_ded.cpp
+++ goldsrc/dedicated/sys_ded.cpp
@@ -1,6 +1,35 @@
+/*
+ *	This file is part of Open GoldSrc Project
+ *	Copyright (C) 2018 Headcrab Garage
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *	In addition, as a special exception, the author gives permission to
+ *	link the code of this program with the Half-Life Game Engine ("GoldSrc/GS
+ *	Engine") and Modified Game Libraries ("MODs") developed by Valve,
+ *	L.L.C ("Valve"). You must obey the GNU General Public License in all
+ *	respects for all of the code used other than the GoldSrc Engine and MODs
+ *	from Valve. If you modify this file, you may extend this exception
+ *	to your version of the file, but you are not obligated to do so. If
+ *	you do not wish to do so, delete this exception statement from your
+ *	version.
+ */
+
 /// @file
 
 #include <cstdio>
+#include <stdexcept>
 
 #ifdef _WIN32
 	#include <windows.h>
@@ -51,7 +80,7 @@ int RunServer() // void?
 	auto pFSLib{Sys_LoadModule("filesystem_stdio")};
 	
 	if(!pFSLib)
-		return EXIT_FAILURE;
+		throw std::runtime_error(std::string("Failed to load the filesystem module!"));
 	
 	auto pFSFactory{Sys_GetFactory(pFSLib)};
 	
@@ -61,7 +90,7 @@ int RunServer() // void?
 	auto pEngineLib{Sys_LoadModule("engine")};
 	
 	if(!pEngineLib)
-		return EXIT_FAILURE;
+		throw std::runtime_error(std::string("Failed to load the engine module!"));
 	
 	auto pEngineFactory{Sys_GetFactory(pEngineLib)};
 	
@@ -87,6 +116,8 @@ int RunServer() // void?
 
 int main(int argc, char **argv)
 {
+	//CCmdLine CmdLine(argc, argv); // TODO
+	
 	if(!InitConsole())
 		return EXIT_FAILURE;
 	
