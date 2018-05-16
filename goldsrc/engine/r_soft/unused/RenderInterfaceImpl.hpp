405ad106ef4ddd943c04154a434b08cb6e1b6aab
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-05-16 13:45:01 +0000

    Update/add license headers for the whole codebase

--- goldsrc/engine/r_soft/unused/RenderInterfaceImpl.hpp
+++ goldsrc/engine/r_soft/unused/RenderInterfaceImpl.hpp
@@ -1,6 +1,6 @@
 /*
  *	This file is part of OGS Engine
- *	Copyright (C) 2017 OGS Dev Team
+ *	Copyright (C) 2017-2018 BlackPhrase
  *
  *	OGS Engine is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -14,17 +14,7 @@
  *
  *	You should have received a copy of the GNU General Public License
  *	along with OGS Engine. If not, see <http://www.gnu.org/licenses/>.
- *
- *	In addition, as a special exception, the author gives permission to
- *	link the code of OGS Engine with the Half-Life Game Engine ("GoldSrc/GS
- *	Engine") and Modified Game Libraries ("MODs") developed by Valve,
- *	L.L.C ("Valve"). You must obey the GNU General Public License in all
- *	respects for all of the code used other than the GoldSrc Engine and MODs
- *	from Valve. If you modify this file, you may extend this exception
- *	to your version of the file, but you are not obligated to do so. If
- *	you do not wish to do so, delete this exception statement from your
- *	version.
-*/
+ */
 
 /// @file
 
