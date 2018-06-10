efd5ff68be75fef1e6069ef79aa82a5d8d653d51
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-06-10 13:19:36 +0000

    [Engine] Partially implement filesystem interface (VFileSystem009) wrapper functions [ci skip]

--- goldsrc/engine/filesystem_internal.cpp
+++ goldsrc/engine/filesystem_internal.cpp
@@ -46,6 +46,271 @@ void FileSystem_Shutdown()
 	gpFileSystem = nullptr;
 };
 
+//////////////////////////////////////////////////////////////
+// VFileSystem009 interface wrapper
+//////////////////////////////////////////////////////////////
+
+//void FS_Mount();
+//void FS_Unmount();
+
+void FS_RemoveAllSearchPaths()
+{
+	gpFileSystem->RemoveAllSearchPaths();
+};
+
+void FS_AddSearchPath(const char *pPath, const char *pathID)
+{
+	gpFileSystem->AddSearchPath(pPath, pathID);
+};
+
+bool FS_RemoveSearchPath(const char *pPath)
+{
+	return gpFileSystem->RemoveSearchPath(pPath);
+};
+
+void FS_RemoveFile(const char *pRelativePath, const char *pathID)
+{
+	gpFileSystem->RemoveFile(pRelativePath, pathID);
+};
+
+void FS_CreateDirHierarchy(const char *path, const char *pathID)
+{
+	gpFileSystem->CreateDirHierarchy(path, pathID);
+};
+
+bool FS_FileExists(const char *pFileName)
+{
+	return gpFileSystem->FileExists(pFileName);
+};
+
+bool FS_IsDirectory(const char *pFileName)
+{
+	return gpFileSystem->IsDirectory(pFileName);
+};
+
+FileHandle_t FS_Open(const char *pFileName, const char *pOptions, const char *pathID)
+{
+	return gpFileSystem->Open(pFileName, pOptions, pathID);
+};
+
+void FS_Close(FileHandle_t file)
+{
+	gpFileSystem->Close(file);
+};
+
+void FS_Seek(FileHandle_t file, int pos, FileSystemSeek_t seekType)
+{
+	gpFileSystem->Seek(file, pos, seekType);
+};
+
+unsigned int FS_Tell(FileHandle_t file)
+{
+	return gpFileSystem->Tell(file);
+};
+
+unsigned int FS_Size(FileHandle_t file)
+{
+	return gpFileSystem->Size(file);
+};
+
+unsigned int FS_Size(const char *pFileName) // TODO: FS_FileSize? C doesn't support function overloading so this won't work otherwise
+{
+	return gpFileSystem->Size(pFileName);
+};
+
+long FS_GetFileTime(const char *pFileName)
+{
+	return gpFileSystem->GetFileTime(pFileName);
+};
+
+void FS_FileTimeToString(char *pStrip, int maxCharsIncludingTerminator, long fileTime)
+{
+	return gpFileSystem->FileTimeToString(pStrip, maxCharsIncludingTerminator, fileTime);
+};
+
+bool FS_IsOk(FileHandle_t file)
+{
+	return gpFileSystem->IsOk(file);
+};
+
+void FS_Flush(FileHandle_t file)
+{
+	gpFileSystem->Flush(file);
+};
+
+bool FS_EndOfFile(FileHandle_t file)
+{
+	return gpFileSystem->EndOfFile(file);
+};
+
+int FS_Read(void *pOutput, int size, FileHandle_t file)
+{
+	return gpFileSystem->Read(pOutput, size, file);
+};
+
+int FS_Write(void const *pInput, int size, FileHandle_t file) // void const
+{
+	return gpFileSystem->Write(pInput, size, file);
+};
+
+char *FS_ReadLine(char *pOutput, int maxChars, FileHandle_t file)
+{
+	return gpFileSystem->ReadLine(pOutput, maxChars, file);
+};
+
+int FS_FPrintf(FileHandle_t file, char *pFormat, ...)
+{
+	// TODO
+	return gpFileSystem->FPrintf(file, pFormat);
+};
+
+void *FS_GetReadBuffer(FileHandle_t file, int *outBufferSize, bool failIfNotInCache)
+{
+	return gpFileSystem->GetReadBuffer(file, outBufferSize, failIfNotInCache);
+};
+
+void FS_ReleaseReadBuffer(FileHandle_t file, void *readBuffer)
+{
+	gpFileSystem->ReleaseReadBuffer(file, readBuffer);
+};
+
+const char *FS_FindFirst(const char *pWildCard, FileFindHandle_t *pHandle, const char *pathID)
+{
+	return gpFileSystem->FindFirst(pWildCard, pHandle, pathID);
+};
+
+const char *FS_FindNext(FileFindHandle_t handle)
+{
+	return gpFileSystem->FindNext(handle);
+};
+
+bool FS_FindIsDirectory(FileFindHandle_t handle)
+{
+	return gpFileSystem->FindIsDirectory(handle);
+};
+
+void FS_FindClose(FileFindHandle_t handle)
+{
+	gpFileSystem->FindClose(handle);
+};
+
+void FS_GetLocalCopy(const char *pFileName)
+{
+	gpFileSystem->GetLocalCopy(pFileName);
+};
+
+const char *FS_GetLocalPath(const char *pFileName, char *pLocalPath, int localPathBufferSize)
+{
+	return gpFileSystem->GetLocalPath(pFileName, pLocalPath, localPathBufferSize);
+};
+
+char *FS_ParseFile(char *pFileBytes, char *pToken, bool *pWasQuoted)
+{
+	return gpFileSystem->ParseFile(pFileBytes, pToken, pWasQuoted);
+};
+
+bool FS_FullPathToRelativePath(const char *pFullpath, char *pRelative)
+{
+	return gpFileSystem->FullPathToRelativePath(pFullpath, pRelative);
+};
+
+bool FS_GetCurrentDirectory(char *pDirectory, int maxlen)
+{
+	return gpFileSystem->GetCurrentDirectory(pDirectory, maxlen);
+};
+
+void FS_PrintOpenedFiles()
+{
+	gpFileSystem->PrintOpenedFiles();
+};
+
+void FS_SetWarningFunc(void (*pfnWarning)(const char *fmt, ...))
+{
+	gpFileSystem->SetWarningFunc(pfnWarning);
+};
+
+void FS_SetWarningLevel(FileWarningLevel_t level)
+{
+	gpFileSystem->SetWarningLevel(level);
+};
+
+void FS_LogLevelLoadStarted(const char *name)
+{
+	gpFileSystem->LogLevelLoadStarted(name);
+};
+
+void FS_LogLevelLoadFinished(const char *name)
+{
+	gpFileSystem->LogLevelLoadFinished(name);
+};
+
+int FS_HintResourceNeed(const char *hintlist, int forgetEverything)
+{
+	return gpFileSystem->HintResourceNeed(hintlist, forgetEverything);
+};
+
+int FS_PauseResourcePreloading()
+{
+	return gpFileSystem->PauseResourcePreloading();
+};
+
+int FS_ResumeResourcePreloading()
+{
+	return gpFileSystem->ResumeResourcePreloading();
+};
+
+int FS_SetVBuf(FileHandle_t stream, char *buffer, int mode, long size)
+{
+	return gpFileSystem->SetVBuf(stream, buffer, mode, size);
+};
+
+void FS_GetInterfaceVersion(char *p, int maxlen)
+{
+	gpFileSystem->GetInterfaceVersion(p, maxlen);
+};
+
+bool FS_IsFileImmediatelyAvailable(const char *pFileName)
+{
+	return gpFileSystem->IsFileImmediatelyAvailable(pFileName);
+};
+
+WaitForResourcesHandle_t FS_WaitForResources(const char *resourcelist)
+{
+	return gpFileSystem->WaitForResources(resourcelist);
+};
+
+bool FS_GetWaitForResourcesProgress(WaitForResourcesHandle_t handle, float *progress, bool *complete)
+{
+	return gpFileSystem->GetWaitForResourcesProgress(handle, progress, complete);
+};
+
+void FS_CancelWaitForResources(WaitForResourcesHandle_t handle)
+{
+	gpFileSystem->CancelWaitForResources(handle);
+};
+
+bool FS_IsAppReadyForOfflinePlay(int appID)
+{
+	return gpFileSystem->IsAppReadyForOfflinePlay(appID);
+};
+
+bool FS_AddPackFile(const char *fullpath, const char *pathID)
+{
+	return gpFileSystem->AddPackFile(fullpath, pathID);
+};
+
+FileHandle_t FS_OpenFromCacheForRead(const char *pFileName, const char *pOptions, const char *pathID)
+{
+	return gpFileSystem->OpenFromCacheForRead(pFileName, pOptions, pathID);
+};
+
+void FS_AddSearchPathNoWrite(const char *pPath, const char *pathID)
+{
+	gpFileSystem->AddSearchPathNoWrite(pPath, pathID);
+};
+
+//////////////////////////////////////////////////////////////
+
 /*
 ===============================================================================
 
--- goldsrc/engine/filesystem_internal.h
+++ goldsrc/engine/filesystem_internal.h
@@ -19,10 +19,6 @@
 /// @file
 /// @brief internal filesystem module wrapper
 
-//
-// file IO
-//
-
 #pragma once
 
 #define FILESYSTEM_INTERNAL_H
@@ -56,6 +52,96 @@ typedef enum
 
 void FileSystem_Init(char *basedir, void *filesystemFactory); // TODO: return bool/int?
 void FileSystem_Shutdown();
+////////////
+
+// VFileSystem009 interface wrapper
+
+//void FS_Mount();
+//void FS_Unmount();
+
+void FS_RemoveAllSearchPaths();
+
+void FS_AddSearchPath(const char *pPath, const char *pathID);
+bool FS_RemoveSearchPath(const char *pPath);
+
+void FS_RemoveFile(const char *pRelativePath, const char *pathID);
+
+void FS_CreateDirHierarchy(const char *path, const char *pathID);
+
+bool FS_FileExists(const char *pFileName);
+bool FS_IsDirectory(const char *pFileName);
+
+FileHandle_t FS_Open(const char *pFileName, const char *pOptions, const char *pathID);
+void FS_Close(FileHandle_t file);
+
+void FS_Seek(FileHandle_t file, int pos, FileSystemSeek_t seekType);
+unsigned int FS_Tell(FileHandle_t file);
+
+unsigned int FS_Size(FileHandle_t file);
+unsigned int FS_Size(const char *pFileName); // TODO: FS_FileSize? C doesn't support function overloading so this won't work otherwise
+
+long FS_GetFileTime(const char *pFileName);
+void FS_FileTimeToString(char *pStrip, int maxCharsIncludingTerminator, long fileTime);
+
+bool FS_IsOk(FileHandle_t file);
+
+void FS_Flush(FileHandle_t file);
+bool FS_EndOfFile(FileHandle_t file);
+
+int FS_Read(void *pOutput, int size, FileHandle_t file);
+int FS_Write(void const *pInput, int size, FileHandle_t file); // void const
+char *FS_ReadLine(char *pOutput, int maxChars, FileHandle_t file);
+int FS_FPrintf(FileHandle_t file, char *pFormat, ...);
+
+void *FS_GetReadBuffer(FileHandle_t file, int *outBufferSize, bool failIfNotInCache);
+void FS_ReleaseReadBuffer(FileHandle_t file, void *readBuffer);
+
+const char *FS_FindFirst(const char *pWildCard, FileFindHandle_t *pHandle, const char *pathID);
+const char *FS_FindNext(FileFindHandle_t handle);
+bool FS_FindIsDirectory(FileFindHandle_t handle);
+void FS_FindClose(FileFindHandle_t handle);
+
+void FS_GetLocalCopy(const char *pFileName);
+
+const char *FS_GetLocalPath(const char *pFileName, char *pLocalPath, int localPathBufferSize);
+
+char *FS_ParseFile(char *pFileBytes, char *pToken, bool *pWasQuoted);
+
+bool FS_FullPathToRelativePath(const char *pFullpath, char *pRelative);
+
+bool FS_GetCurrentDirectory(char *pDirectory, int maxlen);
+
+void FS_PrintOpenedFiles();
+
+void FS_SetWarningFunc(void (*pfnWarning)(const char *fmt, ...));
+void FS_SetWarningLevel(FileWarningLevel_t level);
+
+void FS_LogLevelLoadStarted(const char *name);
+void FS_LogLevelLoadFinished(const char *name);
+
+int FS_HintResourceNeed(const char *hintlist, int forgetEverything);
+
+int FS_PauseResourcePreloading();
+int FS_ResumeResourcePreloading();
+
+int FS_SetVBuf(FileHandle_t stream, char *buffer, int mode, long size);
+
+void FS_GetInterfaceVersion(char *p, int maxlen);
+
+bool FS_IsFileImmediatelyAvailable(const char *pFileName);
+
+WaitForResourcesHandle_t FS_WaitForResources(const char *resourcelist);
+bool FS_GetWaitForResourcesProgress(WaitForResourcesHandle_t handle, float *progress, bool *complete);
+void FS_CancelWaitForResources(WaitForResourcesHandle_t handle);
+
+bool FS_IsAppReadyForOfflinePlay(int appID);
+
+bool FS_AddPackFile(const char *fullpath, const char *pathID);
+
+FileHandle_t FS_OpenFromCacheForRead(const char *pFileName, const char *pOptions, const char *pathID);
+
+void FS_AddSearchPathNoWrite(const char *pPath, const char *pathID);
+
 ////////////
 
 /// returns the file size
