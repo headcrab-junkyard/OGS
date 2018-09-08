2dcd1433f35cebc8bc868797d3a686694e163bc2
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-09-08 14:58:59 +0000

    [Engine] Provide null implementation for client-side prediction functionality

---
+++ goldsrc/engine/cl_pred-null.c
@@ -0,0 +1,30 @@
+/// @file
+
+#include "quakedef.h"
+
+/*
+==============
+CL_PredictUsercmd
+==============
+*/
+void CL_PredictUsercmd(local_state_t *from, local_state_t *to, usercmd_t *u, qboolean spectator) // TODO: player_state_t should be local_state_t
+{
+};
+
+/*
+==============
+CL_PredictMove
+==============
+*/
+void CL_PredictMove()
+{
+};
+
+/*
+==============
+CL_InitPrediction
+==============
+*/
+void CL_InitPrediction()
+{
+};
