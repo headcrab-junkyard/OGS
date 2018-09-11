66fea2522145eb017a406cbbb88dc930cd76ef64
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-09-11 12:50:28 +0000

    [Engine] Implement library loading wrapper functions

---
+++ goldsrc/engine/sys_temp.cpp
@@ -0,0 +1,16 @@
+/// @file
+
+#include "interface.h"
+
+extern "C"
+{
+void *Sys_GetExport_Wrapper(void *apModule, const char *asName)
+{
+	return Sys_GetExport(apModule, asName);
+};
+
+void Sys_UnloadModule_Wrapper(void *apModule)
+{
+	Sys_UnloadModule(apModule);
+};
+};
