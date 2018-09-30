8ca846aed1b73c5b51b5aaceddba5e9e28482255
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-09-30 14:06:15 +0000

    [GameUI] OptionsSubGame, OptionsSubDifficulty and LoadingTipPanel are unused

--- goldsrc/gameui/OptionsSubDifficulty.cpp
+++
@@ -1,6 +0,0 @@
-/// @file
-
-#include "OptionsSubDifficulty.hpp"
-
-COptionsSubDifficulty::COptionsSubDifficulty() = default;
-COptionsSubDifficulty::~COptionsSubDifficulty() = default;
--- goldsrc/gameui/OptionsSubGame.cpp
+++
@@ -1,6 +0,0 @@
-/// @file
-
-#include "OptionsSubGame.hpp"
-
-COptionsSubGame::COptionsSubGame() = default;
-COptionsSubGame::~COptionsSubGame() = default;
