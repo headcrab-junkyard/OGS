844b72f63d0e260fb17ec4809ec98793842d50c1
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-10-04 14:18:12 +0000

    [Engine] Update vguiwrap wrapper (uh...) implementation

--- goldsrc/engine/fixme/vguiwrap.cpp
+++ goldsrc/engine/fixme/vguiwrap.cpp
@@ -15,3 +15,65 @@
  *	You should have received a copy of the GNU General Public License
  *	along with OGS Engine. If not, see <http://www.gnu.org/licenses/>.
  */
+
+/// @file
+
+#include "quakedef.h"
+#include "ivguiwrap.hpp"
+
+void *gpVGuiWrapLib{nullptr};
+IVGuiWrap *gpVGuiWrap{nullptr};
+
+void UnloadVGuiWrapModule()
+{
+	if(gpVGuiWrapLib)
+	{
+		Sys_UnloadModule(gpVGuiWrapLib);
+		gpVGuiWrapLib = nullptr;
+	};
+};
+
+bool LoadVGuiWrapModule()
+{
+	//UnloadVGuiWrapModule();
+	
+	gpVGuiWrapLib = Sys_LoadModule("vguiwrap");
+	
+	if(!gpVGuiWrapLib)
+		return false;
+	
+	auto fnVGuiWrapFactory{Sys_GetFactory(gpVGuiWrapLib)};
+	
+	if(!fnVGuiWrapFactory)
+		return false;
+	
+	gpVGuiWrap = (IVGuiWrap*)fnVGuiWrapFactory(OGS_VGUIWRAP_INTERFACE_VERSION, nullptr);
+	
+	if(!gpVGuiWrap)
+		return false;
+	
+	return true;
+};
+
+void VGuiWrap_Startup()
+{
+	if(!LoadVGuiWrapModule())
+		return;
+	
+	gpVGuiWrap->Startup();
+};
+
+void VGuiWrap_Shutdown()
+{
+	gpVGuiWrap->Shutdown();
+	
+	UnloadVGuiWrapModule();
+};
+
+void *VGuiWrap_GetPanel()
+{
+	if(gpVGuiWrap)
+		return gpVGuiWrap->GetPanel();
+	
+	return nullptr;
+};
