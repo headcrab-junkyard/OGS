a001f1d034b36dc17b07346dbc478baa4106c6c0
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-10-04 11:53:15 +0000

    [Engine] Add partially reimplemented IGame interface

---
+++ goldsrc/engine/igame.h
@@ -0,0 +1,33 @@
+/*
+ *	This file is part of OGS Engine
+ *	Copyright (C) 2018 BlackPhrase
+ *
+ *	OGS Engine is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	OGS Engine is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with OGS Engine. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/// @file
+
+#pragma once
+
+struct IGame
+{
+	virtual void CreateGameWindow() = 0;
+
+	virtual void PlayStartupVideos() = 0;
+	virtual void PlayVideoAndWait(const char *name, bool) = 0;
+
+	virtual void SleepUntilInput(int time) = 0;
+};
+
+extern IGame *gpGame;
