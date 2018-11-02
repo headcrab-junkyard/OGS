802dc35dbaf2e77db27d67e4b1e6fb3d601c71e8
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-11-02 15:32:45 +0000

    [Engine] Update client-side event system implementation

--- goldsrc/engine/cl_parsefn.c
+++ goldsrc/engine/cl_parsefn.c
@@ -19,7 +19,69 @@
 /// @file
 
 #include "quakedef.h"
+#include "event_args.h"
 
-void CL_FireEvents(){
+#define MAX_EVENTS 32
+
+typedef struct event_s
+{
+	unsigned short id;
+	const char *name;
+	event_args_t args;
+	float delay;
+	//pfnEventHook fnHook;
+} event_t;
+
+event_t gEventQue[MAX_EVENTS];
+
+void CL_QueEvent(event_args_t *apArgs, float delay)
+{
+	static int nLast = 0;
+	
+	if(nLast >= MAX_EVENTS)
+		return;
+	
+	gEventQue[nLast].args.flags = apArgs->flags;
+	gEventQue[nLast].args.entindex = apArgs->entindex;
+	
+	VectorCopy(apArgs->origin, gEventQue[nLast].args.origin);
+	VectorCopy(apArgs->angles, gEventQue[nLast].args.angles);
+	VectorCopy(apArgs->velocity, gEventQue[nLast].args.velocity);
+	
+	gEventQue[nLast].args.ducking = apArgs->ducking;
+	
+	gEventQue[nLast].args.fparam1 = apArgs->fparam1;
+	gEventQue[nLast].args.fparam2 = apArgs->fparam2;
+	
+	gEventQue[nLast].args.iparam1 = apArgs->iparam1;
+	gEventQue[nLast].args.iparam2 = apArgs->iparam2;
+	
+	gEventQue[nLast].args.bparam1 = apArgs->bparam1;
+	gEventQue[nLast].args.bparam2 = apArgs->bparam2;
+	
+	gEventQue[nLast].delay = delay;
+	
+	nLast++;
+};
+
+void CL_ParseEvent()
+{
 	// TODO
+	
+	event_args_t args;
+	memset(&args, 0, sizeof(args));
+	
+	CL_QueEvent(&args, 0.0f);
+};
+
+void CL_FireEvents()
+{
+	// TODO
+	
+	//for(int i = 0; i < MAX_EVENTS; i++)
+	{
+		//gEventQue[i]
+		
+		//Con_Printf("CL_FireEvents: Unknown event %d:%s", currentevent.id, currentevent.name);
+	};
 };
