066655eca5064ccf11eb3538fbb2b6d9b658708d
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-11-19 12:44:43 +0000

    [Engine] Update mathlib code (some pieces were moved to ogs-mathlib library)

--- goldsrc/engine/mathlib.c
+++ goldsrc/engine/mathlib.c
@@ -16,12 +16,13 @@
  *	You should have received a copy of the GNU General Public License
  *	along with OGS Engine. If not, see <http://www.gnu.org/licenses/>.
  */
-// mathlib.c -- math primitives
+
+/// @file
+/// @brief math primitives
 
 #include <math.h>
 #include "quakedef.h"
 
-vec3_t vec3_origin = { 0, 0, 0 };
 int nanmask = 255 << 23;
 
 /*-----------------------------------------------------------------*/
@@ -147,18 +148,6 @@ void RotatePointAroundVector(vec3_t dst, const vec3_t dir, const vec3_t point, f
 
 /*-----------------------------------------------------------------*/
 
-float anglemod(float a)
-{
-#if 0
-	if (a >= 0)
-		a -= 360*(int)(a/360);
-	else
-		a += 360*( 1 + (int)(-a/360) );
-#endif
-	a = (360.0 / 65536) * ((int)(a * (65536 / 360.0)) & 65535);
-	return a;
-}
-
 /*
 ==================
 BOPS_Error
@@ -282,138 +271,6 @@ int BoxOnPlaneSide(vec3_t emins, vec3_t emaxs, mplane_t *p)
 }
 
 //#endif // !id386
-
-void AngleVectors(vec3_t angles, vec3_t forward, vec3_t right, vec3_t up)
-{
-	float angle;
-	float sr, sp, sy, cr, cp, cy;
-
-	angle = angles[YAW] * (M_PI * 2 / 360);
-	sy = sin(angle);
-	cy = cos(angle);
-	angle = angles[PITCH] * (M_PI * 2 / 360);
-	sp = sin(angle);
-	cp = cos(angle);
-	angle = angles[ROLL] * (M_PI * 2 / 360);
-	sr = sin(angle);
-	cr = cos(angle);
-
-	forward[0] = cp * cy;
-	forward[1] = cp * sy;
-	forward[2] = -sp;
-	right[0] = (-1 * sr * sp * cy + -1 * cr * -sy);
-	right[1] = (-1 * sr * sp * sy + -1 * cr * cy);
-	right[2] = -1 * sr * cp;
-	up[0] = (cr * sp * cy + -sr * -sy);
-	up[1] = (cr * sp * sy + -sr * cy);
-	up[2] = cr * cp;
-}
-
-int VectorCompare(vec3_t v1, vec3_t v2)
-{
-	int i;
-
-	for(i = 0; i < 3; i++)
-		if(v1[i] != v2[i])
-			return 0;
-
-	return 1;
-}
-
-void VectorMA(vec3_t veca, float scale, vec3_t vecb, vec3_t vecc)
-{
-	vecc[0] = veca[0] + scale * vecb[0];
-	vecc[1] = veca[1] + scale * vecb[1];
-	vecc[2] = veca[2] + scale * vecb[2];
-}
-
-vec_t _DotProduct(vec3_t v1, vec3_t v2)
-{
-	return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
-}
-
-void _VectorSubtract(vec3_t veca, vec3_t vecb, vec3_t out)
-{
-	out[0] = veca[0] - vecb[0];
-	out[1] = veca[1] - vecb[1];
-	out[2] = veca[2] - vecb[2];
-}
-
-void _VectorAdd(vec3_t veca, vec3_t vecb, vec3_t out)
-{
-	out[0] = veca[0] + vecb[0];
-	out[1] = veca[1] + vecb[1];
-	out[2] = veca[2] + vecb[2];
-}
-
-void _VectorCopy(vec3_t in, vec3_t out)
-{
-	out[0] = in[0];
-	out[1] = in[1];
-	out[2] = in[2];
-}
-
-void CrossProduct(vec3_t v1, vec3_t v2, vec3_t cross)
-{
-	cross[0] = v1[1] * v2[2] - v1[2] * v2[1];
-	cross[1] = v1[2] * v2[0] - v1[0] * v2[2];
-	cross[2] = v1[0] * v2[1] - v1[1] * v2[0];
-}
-
-double sqrt(double x);
-
-vec_t Length(vec3_t v)
-{
-	int i;
-	float length;
-
-	length = 0;
-	for(i = 0; i < 3; i++)
-		length += v[i] * v[i];
-	length = sqrt(length); // FIXME
-
-	return length;
-}
-
-float VectorNormalize(vec3_t v)
-{
-	float length, ilength;
-
-	length = v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
-	length = sqrt(length); // FIXME
-
-	if(length)
-	{
-		ilength = 1 / length;
-		v[0] *= ilength;
-		v[1] *= ilength;
-		v[2] *= ilength;
-	}
-
-	return length;
-}
-
-void VectorInverse(vec3_t v)
-{
-	v[0] = -v[0];
-	v[1] = -v[1];
-	v[2] = -v[2];
-}
-
-void VectorScale(vec3_t in, vec_t scale, vec3_t out)
-{
-	out[0] = in[0] * scale;
-	out[1] = in[1] * scale;
-	out[2] = in[2] * scale;
-}
-
-int Q_log2(int val)
-{
-	int answer = 0;
-	while(val >>= 1) // TODO:  while((val>>=1) != 0)
-		answer++;
-	return answer;
-}
 
 /*
 ================
