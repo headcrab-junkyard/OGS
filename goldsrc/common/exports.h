d234b96ddb1600f06b427b5deb77d0d2c1b6c6af
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-12-08 10:28:22 +0000

    Add exports header (common to both client and game dlls)

---
+++ goldsrc/common/exports.h
@@ -0,0 +1,31 @@
+/*
+ * This file is part of OGS Engine
+ * Copyright (C) 2018 BlackPhrase
+ *
+ * OGS Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OGS Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OGS Engine. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/// @file
+
+#pragma once
+
+#ifdef _WIN32
+	#define EXPORT [[dllexport]]
+#else
+	#define EXPORT [[visibility("default")]]
+#endif
+
+#define C_EXPORT extern "C" EXPORT
+
+//typedef struct globalvars_s globalvars_t;
