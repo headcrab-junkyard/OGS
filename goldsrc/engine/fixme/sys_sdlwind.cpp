bac29843db809ada65f2bbe3754c7c4a0b95324c
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-04-11 11:13:29 +0000

    [Engine] Update SDL2 IGame interface implementation (window opening code)

--- goldsrc/engine/fixme/sys_sdlwind.cpp
+++ goldsrc/engine/fixme/sys_sdlwind.cpp
@@ -1,6 +1,6 @@
 /*
  *	This file is part of OGS Engine
- *	Copyright (C) 2018 BlackPhrase
+ *	Copyright (C) 2018-2019 BlackPhrase
  *
  *	OGS Engine is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
@@ -18,27 +18,77 @@
 
 /// @file
 
-class CGame
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+#include "quakedef.h"
+#include "igame.h"
+
+class CGame final : public IGame
 {
 public:
 	CGame();
 	~CGame();
 
-	void CreateGameWindow();
+	void CreateGameWindow() override;
 
-	void PlayStartupVideos();
-	void PlayVideoAndWait(const char *name, bool);
+	void PlayStartupVideos() override;
+	void PlayVideoAndWait(const char *name, bool) override;
 
-	void SleepUntilInput();
+	void SleepUntilInput(int time) override;
+private:
+	SDL_GLContext mGLContext;
 };
 
 CGame::CGame() = default;
-CGame::~CGame() = default;
 
-void CGame::CreateGameWindow(){};
+CGame::~CGame() //= default;
+{
+	SDL_GL_DeleteContext(mGLContext);
+	SDL_DestroyWindow(mainwindow);
+	SDL_Quit();
+};
 
-void CGame::PlayStartupVideos(){};
+void CGame::CreateGameWindow()
+{
+	// Init SDL2
+	if(SDL_WasInit(SDL_INIT_VIDEO | SDL_INIT_TIMER) == 0)
+		if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+			Sys_Error("Couldn't create SDL2 window! (%s)", SDL_GetError());
+	
+	// Setup window attributes
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+	
+	SDL_DisplayMode CurrentDisplayMode;
+	SDL_GetCurrentDisplayMode(0, &CurrentDisplayMode);
+	
+	// TODO: fullscreen support
+	
+	SDL_WindowFlags WindowFlags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI;
+	mainwindow = SDL_CreateWindow("OGS Engine Tech Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 600, WindowFlags);
+	
+	mGLContext = SDL_GL_CreateContext(mainwindow);
+	
+	SDL_GL_SetSwapInterval(1);
+	
+	// TODO: renderer
+};
 
-void CGame::PlayVideoAndWait(const char *name, bool){};
+void CGame::PlayStartupVideos()
+{
+	// TODO
+};
 
-void CGame::SleepUntilInput(){};
+void CGame::PlayVideoAndWait(const char *name, bool)
+{
+	// TODO
+};
+
+void CGame::SleepUntilInput(int time)
+{
+	// TODO
+};
