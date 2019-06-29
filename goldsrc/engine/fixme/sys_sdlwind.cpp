157441a530c33d5a549aa88e4a0bcac8802e169f
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-06-29 10:35:43 +0000

    [Engine] Update SDL2 window creation code

--- goldsrc/engine/fixme/sys_sdlwind.cpp
+++ goldsrc/engine/fixme/sys_sdlwind.cpp
@@ -1,25 +1,27 @@
 /*
- *	This file is part of OGS Engine
- *	Copyright (C) 2018-2019 BlackPhrase
+ * This file is part of OGS Engine
+ * Copyright (C) 2018-2019 BlackPhrase
  *
- *	OGS Engine is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
+ * OGS Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *	OGS Engine is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *	GNU General Public License for more details.
+ * OGS Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with OGS Engine. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with OGS Engine. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /// @file
 
 #include <SDL.h>
 #include <SDL_opengl.h>
+#include <SDL_syswm.h>
+#include <stb_image.h>
 
 #include "quakedef.h"
 #include "igame.h"
@@ -38,13 +40,21 @@ public:
 	void SleepUntilInput(int time) override;
 private:
 	SDL_GLContext mGLContext;
+	
+	SDL_Window *mpWindow{nullptr};
+	SDL_Cursor *mpCursor{nullptr};
 };
 
 CGame::CGame() = default;
 
 CGame::~CGame() //= default;
 {
-	SDL_GL_DeleteContext(mGLContext);
+	SDL_FreeCursor(mpCursor);
+	
+	SDL_FreeSurface(pWinIcon);
+	stbi_image_free(pGameIcon);
+	
+	SDL_GL_DeleteContext(mGLContext); // TODO: unused?
 	SDL_DestroyWindow(mainwindow);
 	SDL_Quit();
 };
@@ -53,7 +63,7 @@ void CGame::CreateGameWindow()
 {
 	// Init SDL2
 	if(SDL_WasInit(SDL_INIT_VIDEO | SDL_INIT_TIMER) == 0)
-		if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0)
+		if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER) != 0) // TODO: SDL_InitSubsystem?
 			Sys_Error("Couldn't create SDL2 window! (%s)", SDL_GetError());
 	
 	// Setup window attributes
@@ -66,16 +76,101 @@ void CGame::CreateGameWindow()
 	SDL_DisplayMode CurrentDisplayMode;
 	SDL_GetCurrentDisplayMode(0, &CurrentDisplayMode);
 	
-	// TODO: fullscreen support
+	SDL_WindowFlags WindowFlags{SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI}; // TODO: check if we should add opengl to flags
 	
-	SDL_WindowFlags WindowFlags = SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI;
-	mainwindow = SDL_CreateWindow("OGS Engine Tech Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 600, WindowFlags);
+	if(!COM_CheckParm("-noontop"))
+		WindowFlags |= SDL_WINDOW_ALWAYS_ON_TOP; //SDL_ALLOW_TOPMOST
 	
-	mGLContext = SDL_GL_CreateContext(mainwindow);
+	if(COM_CheckParm("-noborder"))
+		WindowFlags |= SDL_WINDOW_BORDERLESS;
 	
-	SDL_GL_SetSwapInterval(1);
+	if(COM_CheckParm("-fullscreen"))
+		WindowFlags |= SDL_WINDOW_FULLSCREEN_DESKTOP; // SDL_WINDOW_FULLSCREEN
 	
-	// TODO: renderer
+	mpWindow = SDL_CreateWindow("OGS Engine Tech Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 600, WindowFlags); // TODO: title should be "Half-Life"
+
+	if(!mpWindow)
+		Sys_Error("Failed to create SDL Window! (%s)", SDL_GetError());
+	
+	// BP: hack to make some Windows stuff (dependent on window) work with SDL
+#ifdef _WIN32
+	SDL_SysWMinfo WindowInfo{};
+	SDL_VERSION(&WindowInfo.version);
+	SDL_GetWindowWMInfo(mpWindow, &WindowInfo);
+	
+	mainwindow = WindowInfo.win.window;
+#endif
+	
+	mGLContext = SDL_GL_CreateContext(mpWindow);
+	
+	int r, g, b, a, depth;
+	
+	if(!SDL_GL_GetAttribute(SDL_GL_RED_SIZE, &r))
+		Sys_Error("Failed to get GL RED size (%s)", SDL_GetError());
+	if(!SDL_GL_GetAttribute(SDL_GL_GREEN_SIZE, &g))
+		Sys_Error("Failed to get GL GREEN size (%s)", SDL_GetError());
+	if(!SDL_GL_GetAttribute(SDL_GL_BLUE_SIZE, &b))
+		Sys_Error("Failed to get GL BLUE size (%s)", SDL_GetError());
+	if(!SDL_GL_GetAttribute(SDL_GL_ALPHA_SIZE, &a))
+		Sys_Error("Failed to get GL ALPHA size (%s)", SDL_GetError());
+	if(!SDL_GL_GetAttribute(SDL_GL_DEPTH_SIZE, &depth))
+		Sys_Error("Failed to get GL DEPTH size (%s)", SDL_GetError());
+	
+	Con_DPrintf("GL_SIZES: r:%d g:%d b:%d a:%d depth:%d\n", r, g, b, a, depth);
+	
+	SDL_GL_SetSwapInterval(1); // TODO: "-vsync"?
+	
+	auto req_format{STBI_rgb_alpha};
+	int width, height, orig_format;
+	auto pGameIcon{stbi_load(va("./%s/game.ico", com_gamedir), &width, &height, &orig_format, req_format)};
+	
+	if(!pGameIcon)
+	{
+		SDL_Log("Loading image failed: %s", stbi_failure_reason());
+		exit(1);
+	};
+	
+	// Set up the pixel format color masks for RGB(A) byte arrays.
+	// Only STBI_rgb (3) and STBI_rgb_alpha (4) are supported here!
+	Uint32 rmask, gmask, bmask, amask;
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+	int shift = (req_format == STBI_rgb) ? 8 : 0;
+	rmask = 0xff000000 >> shift;
+	gmask = 0x00ff0000 >> shift;
+	bmask = 0x0000ff00 >> shift;
+	amask = 0x000000ff >> shift;
+#else // little endian, like x86
+	rmask = 0x000000ff;
+	gmask = 0x0000ff00;
+	bmask = 0x00ff0000;
+	amask = (req_format == STBI_rgb) ? 0 : 0xff000000;
+#endif
+
+	int depth, pitch;
+	if(req_format == STBI_rgb)
+	{
+		depth = 24;
+		pitch = 3 * width; // 3 bytes per pixel * pixels per row
+	}
+	else // STBI_rgb_alpha (RGBA)
+	{
+		depth = 32;
+		pitch = 4 * width;
+	};
+	
+	auto pWinIcon{SDL_CreateRGBSurfaceFrom(static_cast<void*>(pGameIcon), width, height, depth, pitch, rmask, gmask, bmask, amask)};
+	
+	if(!pWinIcon)
+	{
+		SDL_Log("Creating surface failed: %s", SDL_GetError());
+		stbi_image_free(pGameIcon);
+		exit(1);
+	};
+	
+	SDL_SetWindowIcon(mpWindow, pWinIcon);
+	
+	mpCursor = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_ARROW);
+	SDL_SetCursor(mpCursor);
 };
 
 void CGame::PlayStartupVideos()
