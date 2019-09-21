bf457f41ba7839f4d3da04a7161cb9c76b7efcd7
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-09-21 13:36:34 +0000

    [GameDLL] Implement a separate entity for ammo_9mmbox (CAmmo9MMBox)

--- goldsrc/dlls/ammo_9mm.cpp
+++ goldsrc/dlls/ammo_9mm.cpp
@@ -13,8 +13,6 @@ private:
 LINK_ENTITY_TO_CLASS(ammo_glockclip, CAmmo9MMClip)
 LINK_ENTITY_TO_CLASS(ammo_9mmclip, CAmmo9MMClip)
 
-LINK_ENTITY_TO_CLASS(ammo_9mmbox, CAmmo9MM)
-
 void CAmmo9MMClip::PostSpawn()
 {
 	SetModel("models/w_9mmclip.mdl");
@@ -23,4 +21,24 @@ void CAmmo9MMClip::PostSpawn()
 bool CAmmo9MMClip::GiveTo(CBasePlayer *apPlayer)
 {
 	return true;
+};
+
+class CAmmo9MMBox : public CBaseAmmo
+{
+public:
+	bool GiveTo(CBasePlayer *apPlayer) override;
+private:
+	void PostSpawn() override;
+};
+
+LINK_ENTITY_TO_CLASS(ammo_9mmbox, CAmmo9MMBox)
+
+void CAmmo9MMBox::PostSpawn()
+{
+	SetModel("models/w_9mmbox.mdl");
+};
+
+bool CAmmo9MMBox::GiveTo(CBasePlayer *apPlayer)
+{
+	return false;
 };
