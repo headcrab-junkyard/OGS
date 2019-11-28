76ebad89ccb7f6fb1ca1c05e39c1fabc1ce41b56
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-11-28 23:29:53 +0000

    Initial IVoiceCodec interface

---
+++ goldsrc/public/voice/ivoicecodec.h
@@ -0,0 +1,34 @@
+/*
+ * This file is part of OGS Engine
+ * Copyright (C) 2018-2019 BlackPhrase
+ *
+ * OGS Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OGS Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OGS Engine. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/// @file
+
+#pragma once
+
+#include "tier1/interface.h"
+
+constexpr auto OGS_VOICECODEC_INTERFACE_VERSION{"IOGSVoiceCodec001"};
+
+struct IVoiceCodec : public IBaseInterface
+{
+	///
+	virtual bool Init(int anQuality) = 0;
+	
+	///
+	virtual void Shutdown() = 0;
+};
