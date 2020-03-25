f34124f04af41661f8bc8ef21697faf3db6e88cc
BlackPhrase <BlackPhrase@users.noreply.github.com>
2020-03-25 11:26:46 +0000

    [Common] Rewritten clientdata_t and local_state_t structs

--- goldsrc/common/entity_state.h
+++ goldsrc/common/entity_state.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of OGS Engine
  * Copyright (C) 1996-1997 Id Software, Inc.
- * Copyright (C) 2018 BlackPhrase
+ * Copyright (C) 2018-2019 BlackPhrase
  *
  * OGS Engine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,6 +22,9 @@
 #pragma once
 
 #include "mathlib.h"
+#include "weaponinfo.h"
+
+#define MAX_WEAPONS 64
 
 /// entity_state_t is the information conveyed from the server
 /// in an update message
@@ -40,3 +43,23 @@ typedef struct entity_state_s
 	
 	int		eflags;			///< nolerp, etc
 } entity_state_t;
+typedef struct clientdata_s
+{
+	vec3_t origin;
+	vec3_t velocity;
+	
+	int flags; // dead, gib, etc
+	int waterlevel;
+	int watertype;
+	vec3_t view_ofs;
+	float health; // cl.stats[STAT_HEALTH]
+	
+	float waterjumptime;
+} clientdata_t;
+
+typedef struct local_state_s
+{
+	entity_state_t playerstate;
+	clientdata_t client;
+	weapon_data_t weapondata[MAX_WEAPONS];
+} local_state_t;
