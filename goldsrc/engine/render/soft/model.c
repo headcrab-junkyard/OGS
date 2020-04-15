6c7346680598f19d70ee242c2e946facbd881491
BlackPhrase <BlackPhrase@users.noreply.github.com>
2020-04-15 12:41:12 +0000

    [Engine/Render-Soft] Set the max cached models limit to 512

--- goldsrc/engine/render/soft/model.c
+++ goldsrc/engine/render/soft/model.c
@@ -34,7 +34,7 @@ model_t *Mod_LoadModel(model_t *mod, qboolean crash);
 
 byte mod_novis[MAX_MAP_LEAFS / 8];
 
-#define MAX_MOD_KNOWN 256
+#define MAX_MOD_KNOWN 512
 model_t mod_known[MAX_MOD_KNOWN];
 int mod_numknown;
 
