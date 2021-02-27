5300e9adf73f3d501b047fab9838c77e930b4f9e
BlackPhrase <BlackPhrase@users.noreply.github.com>
2021-02-27 18:43:31 +0000

    [Engine] Update some of the license headers

--- goldsrc/engine/host_cmd.c
+++ goldsrc/engine/host_cmd.c
@@ -1,20 +1,20 @@
 /*
- *	This file is part of OGS Engine
- *	Copyright (C) 1996-1997 Id Software, Inc.
- *	Copyright (C) 2018-2019 BlackPhrase
+ * This file is part of OGS Engine
+ * Copyright (C) 1996-1997 Id Software, Inc.
+ * Copyright (C) 2018-2020 BlackPhrase
  *
- *	OGS Engine is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
+ * OGS Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *	OGS Engine is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *	GNU General Public License for more details.
+ * OGS Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with OGS Engine. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with OGS Engine. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /// @file
