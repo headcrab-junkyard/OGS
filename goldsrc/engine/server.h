242b84286aa2ec387e065546d03586d877b212ff
BlackPhrase <BlackPhrase@users.noreply.github.com>
2021-10-03 11:57:31 +0000

    [Engine] Update/add the license headers

--- goldsrc/engine/server.h
+++ goldsrc/engine/server.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of OGS Engine
  * Copyright (C) 1996-1997 Id Software, Inc.
- * Copyright (C) 2018-2020 BlackPhrase
+ * Copyright (C) 2018-2021 BlackPhrase
  *
  * OGS Engine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
