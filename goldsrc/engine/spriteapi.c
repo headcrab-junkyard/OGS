b921708e20f496712d3a5836f81ccd0c9799e8d4
BlackPhrase <BlackPhrase@users.noreply.github.com>
2021-10-12 13:19:31 +0000

    [Engine] Update the sprite API impl

--- goldsrc/engine/spriteapi.c
+++ goldsrc/engine/spriteapi.c
@@ -21,9 +21,51 @@
 #include "quakedef.h"
 #include "spriteapi.h"
 
+SpriteHandle_t ghCurrentSprite{NULL};
+vec3_t gvSpriteColor;
+
+qboolean gbScissorActive{false};
+vec2_t gvScissorPos;
+vec2_t gvScissorSize;
+
+mspriteframe_t *SPR_GetFrame(SpriteHandle_t ahSprite, int anFrame)
+{
+	// TODO
+	return NULL;
+};
+
 SpriteHandle_t SPR_Load(const char *asName)
 {
 	// TODO
+	/*
+	FileHandle_t pSpriteFile = FS_Open("asName", "rb");
+	
+	if(!pSpriteFile)
+		return 0;
+	
+	dsprite_t *pin = (sprite_t*)Z_Malloc(sizeof(dsprite_t));
+	
+	int version = LittleLong(pin->version);
+	
+	if(version != SPRITE_VERSION)
+		return 0;
+	
+	numframes = LittleLong(pin->numframes);
+	
+	int size = sizeof(msprite_t) + (numframes - 1) * sizeof(psprite->frames);
+	
+	msprite_t *psprite = Hunk_AllocName(size, asName);
+	
+	psprite->type = LittleLong(pin->type);
+	psprite->maxwidth = LittleLong(pin->width);
+	psprite->maxheight = LittleLong(pin->height);
+	psprite->beamlength = LittleFloat(pin->beamlength);
+	psprite->numframes = numframes;
+	
+	Z_Free(pin);
+	
+	FS_Close(pSpriteFile);
+	*/
 	return 0;
 };
 
@@ -35,19 +77,31 @@ int SPR_Frames(SpriteHandle_t ahSprite)
 
 int SPR_Height(SpriteHandle_t ahSprite, int anFrame)
 {
-	// TODO
+	//bi_trace();
+	
+	mspriteframe_t *pFrame = SPR_GetFrame(ahSprite, anFrame);
+	if(pFrame)
+		return pFrame->height;
+	
 	return 0;
 };
 
 int SPR_Width(SpriteHandle_t ahSprite, int anFrame)
 {
-	// TODO
+	mspriteframe_t *pFrame = SPR_GetFrame(ahSprite, anFrame);
+	if(pFrame)
+		return pFrame->width;
+	
 	return 0;
 };
 
 void SPR_Set(SpriteHandle_t ahSprite, int r, int g, int b)
 {
-	// TODO
+	ghCurrentSprite = ahSprite;
+	
+	gvSpriteColor[0] = r;
+	gvSpriteColor[1] = g;
+	gvSpriteColor[2] = b;
 };
 
 void SPR_Draw(int frame, int x, int y, const wrect_t *pRect)
@@ -67,12 +121,18 @@ void SPR_DrawAdditive(int frame, int x, int y, const wrect_t *pRect)
 
 void SPR_EnableScissor(int x, int y, int width, int height)
 {
-	// TODO
+	gbScissorActive = true;
+	
+	gvScissorPos[0] = x;
+	gvScissorPos[1] = y;
+	
+	gvScissorSize[0] = width;
+	gvScissorSize[1] = height;
 };
 
 void SPR_DisableScissor()
 {
-	// TODO
+	gbScissorActive = false;
 };
 
 client_sprite_t *SPR_GetList(char *psz, int *apnCount)
